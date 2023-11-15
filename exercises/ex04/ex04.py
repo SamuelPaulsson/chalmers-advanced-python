8f8f0976db5ec17547ce3cdbb17849967d0a8949
SamuelPaulsson <samuel.paulsson00@gmail.com>
2023-11-15 13:36:21 +0100

    k

--- exercises/ex04/ex04.py
+++ exercises/ex04/ex04.py
@@ -2,20 +2,37 @@
 
 class Account:
     # an account has a number and a balance
+    def __init__(self, number, balance):
+        self.number = number
+        self.balance = balance
 
     def get_balance(self):
         # return the actual balance
-        pass
+        return self.balance
+
+    def get_number(self):
+        return self.number
 
     def set_balance(self, b):
         # change the balance to b
-        pass
+        self.balance = b
 
 def transfer(account1,account2,amount):
+    balance1 = account1.get_balance()
+    if amount > balance1:
+        print('not enugh balance')
+    else:
+        account1.set_balance(balance1-amount)
+        account2.set_balance(account2.ger_balance()+amount)
+
+a = Account(1, 300)
+b = Account(2,200)
+
+transfer(a,b, 200)
+
     # transfer amount from account1 to account2
     # report "OK" if successful, i.e. account1 has the required amount
     # "not enough money" otherwise
-    pass
 
 ## Question 2 and 3: trees
 import sys
